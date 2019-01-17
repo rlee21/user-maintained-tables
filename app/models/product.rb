3263b21124242c829735395c3870617ca4955056
rlee21 <rlee21@comcast.net>
2019-01-17 03:03:47 -0800

    refactor

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,24 +4,28 @@ class Product < ApplicationRecord
 
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
-      cols = column_names
-      cols = cols - ["created_at", "updated_at"]
-      csv << cols
+      header = ['id', 'product_name', 'reporting_name']
+      csv.add_row(header)
       # require 'pry'; binding.pry
       all.each do |product|
-        csv << product.attributes.values_at(*cols)
+        # csv << product.attributes.values_at(*cols)
+        values = product.attributes.values_at(*header)
+        # values = product.attributes.values
+        csv.add_row(values)
       end
     end
   end
 
-  def self.accessible_attributes
-    ['id', 'product_name', 'reporting_name', 'created_at', 'updated_at']
-  end
+  # def self.accessible_attributes
+  #   # ['id', 'product_name', 'reporting_name', 'created_at', 'updated_at']
+  #   ['id', 'product_name', 'reporting_name']
+  # end
 
   def self.import(file)
     CSV.foreach(file.path, headers: true ) do |row|
       product = find_by_id(row["id"]) || new
-      product.attributes = row.to_hash.slice(*accessible_attributes)
+      # product.attributes = row.to_hash.slice(*accessible_attributes)
+      product.attributes = row.to_hash.slice(*row.to_hash.keys())
       product.save!
     end
   end
