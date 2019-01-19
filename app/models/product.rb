545a5160fa1da7012678eda5624252e82b8cfe90
rlee21 <rlee21@comcast.net>
2019-01-19 14:07:07 -0800

    refactor product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,27 +5,19 @@ class Product < ApplicationRecord
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
       header = ['id', 'product_name', 'reporting_name']
+      require 'pry'; binding.pry
       csv.add_row(header)
-      # require 'pry'; binding.pry
       all.each do |product|
-        # csv << product.attributes.values_at(*cols)
         values = product.attributes.values_at(*header)
-        # values = product.attributes.values
         csv.add_row(values)
       end
     end
   end
 
-  # def self.accessible_attributes
-  #   # ['id', 'product_name', 'reporting_name', 'created_at', 'updated_at']
-  #   ['id', 'product_name', 'reporting_name']
-  # end
-
   def self.import(file)
     CSV.foreach(file.path, headers: true ) do |row|
       product = find_by_id(row["id"]) || new
-      # product.attributes = row.to_hash.slice(*accessible_attributes)
-      product.attributes = row.to_hash.slice(*row.to_hash.keys())
+      product.attributes = row.to_hash
       product.save!
     end
   end
