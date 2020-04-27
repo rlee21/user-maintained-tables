16745b98bd86e25e75c5aacf126a712123bb9064
rlee21 <relee@avvo.com>
2020-04-27 16:21:46 -0700

    imp store

---
+++ TODO.md
@@ -0,0 +1,1 @@
+https://3rd-edition.railstutorial.org/book/log_in_log_out
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
   resources :users
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
